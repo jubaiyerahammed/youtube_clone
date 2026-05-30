ec7872a9de069642753a7c4ffbf460eb693a764f
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-05-30 18:50:47 +0900

    youtube_clone

--- home/views.py
+++ home/views.py
@@ -2,18 +2,23 @@ from django.views import View
 from django.shortcuts import render
 from home.models import TagTitels
 from contents.models import VideoContent
+from shorts.models import Short
 
 class HomeView(View):
     def get(self, request):
         thumnails = ['Recent Videos', 'Playlist', 'Subcription']
         categories = TagTitels.objects.all()
         videos = VideoContent.objects.all().order_by('-created_at')
+        shorts = Short.objects.all()
+
 
         return render(request, 'home/home.html', {
             'thumnails': thumnails,
             'categories': categories,
-            'videos': videos
+            'videos': videos,
+            'shorts':shorts
         })
+    
 
 
 class HomeRecommandView(View):
--- shorts/models.py
+++ shorts/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+
 
 # Create your models here.
+from django.db import models
+
+class Short(models.Model):
+    title = models.CharField(max_length=100)
+    views = models.CharField(max_length=50)
+    thumbnail = models.ImageField(upload_to='shorts/', max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True,)
+
+    video_url = models.URLField(blank=True, null=True) 
+
+    
+    def __str__(self):
+        return self.title
